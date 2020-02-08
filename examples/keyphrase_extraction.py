341d4162dc109fd272143a250cd970a7aa941093
alireza <alirezah320@yahoo.com>
2020-02-09 02:53:04 +0330

    Add requirements.txt

--- examples/keyphrase_extraction.py
+++ examples/keyphrase_extraction.py
@@ -1,16 +1,17 @@
 import perke
-# initialize keyphrase extraction model, here TopicRank
-extractor = perke.unsupervised.TextRank()
 
-# load the content of the document, here document is expected to be in raw
-# format (i.e. a simple text file) and preprocessing is carried out using spacy
+# Initialize keyphrase extraction model, here TopicRank
+extractor = perke.unsupervised.TopicRank()
+
+# Load the content of the document, here document is expected to be in raw
+# format (i.e. a simple text file) and preprocessing is carried out using hazm
 extractor.load_document(input='input.txt')
 
-# keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+# Keyphrase candidate selection, in the case of TopicRank: sequences of nouns
 # and adjectives (i.e. `(Noun|Adj)*`)
 extractor.candidate_selection()
 
-# candidate weighting, in the case of TopicRank: using a random walk algorithm
+# Candidate weighting, in the case of TopicRank: using a random walk algorithm
 extractor.candidate_weighting()
 
 # N-best selection, keyphrases contains the 10 highest scored candidates as
