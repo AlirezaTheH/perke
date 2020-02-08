c3e1df123b20acf128d90bf44aca829de0ae0f22
alireza <alirezah320@yahoo.com>
2020-02-09 03:10:48 +0330

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='perke',
+      version='0.1.0',
+      description='Persian Keyphrase Extraction module',
+      author='Alireza Hosseini',
+      author_email='alirezah320@yahoo.com',
+      license='gnu',
+      packages=['perke', 'pke.unsupervised',
+                'pke.unsupervised.graph_based'],
+      url="https://github.com/alirezah320/perke",
+      install_requires=[
+          'hazm',
+          'networkx',
+          'scipy',
+      ],
+      package_data={'perke': ['resources/*.model']}
+      )
