eff2aa60713ad4a3b2babb60cf0f64aa6921e81d
Alireza <alirezah320@yahoo.com>
2021-04-01 00:34:53 +0430

    Fix setup bug

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup(name='perke',
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 3'
-          'Programming Language :: Python :: 3 :: Only'
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3 :: Only',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
