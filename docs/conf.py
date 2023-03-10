d87b63f9eb157d0871764e7b31f3baac2027a8b7
Alireza <alirezatheh@gmail.com>
2023-03-10 12:47:54 +0330

    Change documentation theme to Furo

--- docs/conf.py
+++ docs/conf.py
@@ -1,46 +1,41 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a
-# full list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another
-# directory, add these directories to sys.path here. If the directory is
-# relative to the documentation root, use os.path.abspath to make it
-# absolute, like shown here.
-
 import re
 import sys
-from os.path import dirname, join
+from pathlib import Path
 
-root = dirname(dirname(__file__))
-sys.path.insert(0, root)
+# -- Path setup --------------------------------------------------------
+root = Path(__file__).parent.parent
+sys.path.insert(0, str(root))
+
 
 # -- Project information -----------------------------------------------
-
 project = 'perke'
 copyright = '2020-Present, Alireza Hosseini'
 author = 'Alireza Hosseini'
 
 # The short X.Y version.
-with open(join(root, 'perke', 'version.py')) as f:
-    version_pattern = re.compile(r'__version__\s+=\s+\'(?P<version>.*)\'')
-    version = re.search(version_pattern, f.read()).group('version')
-
-# The full version, including alpha/beta/rc tags
-release = version
+with open(root / 'perke' / 'version.py') as f:
+    __version__ = re.search(
+        r'__version__\s+=\s+\'(?P<version>.*)\'', f.read()
+    ).group('version')
+    # The short X.Y version.
+    version = '.'.join(__version__.split('.')[:2])
+    # The full version, including alpha/beta/rc tags.
+    release = __version__
 
 
 # -- General configuration ---------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'myst_parser',
+    'sphinx_copybutton',
+    'docs.typer_ext',
+]
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx']
+# Autodoc configs
+autodoc_typehints = 'both'
+autodoc_typehints_description_target = 'documented'
 
 # Napoleon configs
 napoleon_use_ivar = True
@@ -50,28 +45,12 @@ napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = True
 napoleon_use_rtype = False
 
-# Change the default role so we can avoid prefixing everything
-# with :obj:
+# Change the default role, so we can avoid prefixing everything with
+# :obj:
 default_role = 'py:obj'
-
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation
-# for a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets)
-# here, relative to this directory. They are copied after the builtin
-# static files, so a file named "default.css" will overwrite the builtin
-# "default.css".
-html_static_path = ['_static']
-html_css_files = ['css/custom.css']
+html_theme = 'furo'
+html_title = "Perke's documentation"
